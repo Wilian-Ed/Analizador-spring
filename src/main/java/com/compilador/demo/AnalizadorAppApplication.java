08d3d6e060f4956484a5c6e9a0ded36c8e1c6224
Wilian Soch <55908367+Wilian-Ed@users.noreply.github.com>
2021-04-11 22:17:42 -0600

    creando proyecto

---
+++ src/main/java/com/compilador/demo/AnalizadorAppApplication.java
@@ -0,0 +1,13 @@
+package com.compilador.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalizadorAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalizadorAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/compilador/demo/AnalizadorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compilador.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalizadorAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
